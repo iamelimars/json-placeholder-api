b5b29d7a5cb1addc33ea51499733f4b1204ea937
Elisha Marshall <iamelimars@yahoo.com>
2020-02-19 23:06:11 -0500

    Adjusted cors

--- pages/api/profiles.js
+++ pages/api/profiles.js
@@ -2,6 +2,7 @@ import * as profiles from '../../data/profiles.json'
 
 export default (req, res) => {
     setTimeout(() => {
+        res.setHeader('Access-Control-Allow-Origin', '*')
         res.status(200).json(profiles)
         
     }, 5000);
